3054f136d61df37cdfa8790408c45d4cee4cd73f
smartalan91 <61645318+smartalan91@users.noreply.github.com>
2025-09-20 18:57:04 +0800

    game

--- app/blueprints/game/__init__.py
+++
@@ -1,1 +0,0 @@
-from .routes import game_bp
--- app/blueprints/game/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app, Blueprint
-
-game_bp = Blueprint("auth", __name__)
-
-@game_bp.route("/game")
-def index():
-    return "Hello"
